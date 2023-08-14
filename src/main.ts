f2f998ea7a7ac27df1abc167edd77b289796fb78
NFiveDev <nmhjort7@gmail.com>
2023-08-14 21:56:08 +0200

    Layouts work

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { ConfigOptions } from 'express-handlebars/types';
+import { create } from 'express-handlebars';
+
+const hbsConfiguration: ConfigOptions = {
+  partialsDir: join(__dirname, '..', 'views', 'partials'),
+  extname: 'hbs',
+  layoutsDir: join(__dirname, '..', 'views', 'layouts'),
+};
+
+const hbs = create(hbsConfiguration);
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -9,6 +19,7 @@ async function bootstrap() {
     prefix: '/static',
   });
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.engine('hbs', hbs.engine);
   app.setViewEngine('hbs');
   await app.listen(3000);
 }
