b907b784369be5f926550103baadc1fd1e6795d2
NFiveDev <nmhjort7@gmail.com>
2023-08-19 09:40:46 +0200

    user service

--- src/user/service/user.service.ts
+++ src/user/service/user.service.ts
@@ -3,11 +3,19 @@ import { InjectModel } from '@nestjs/mongoose';
 import { User } from '../schema/user.schema';
 import { Model } from 'mongoose';
 
+interface UserDto {
+    username: string,
+    password: string,
+    email: string,
+    role?: string
+}
+
 @Injectable()
 export class UserService {
     constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
-    async create(): Promise<User> {
-        
+    async create(userDto : UserDto): Promise<User> {
+        const created = new this.userModel(userDto);
+        return created.save();
     }
 }
